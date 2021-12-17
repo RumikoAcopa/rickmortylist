996a1e36c2754f0fe4803d69cad098b76ebf0e54
rumikoacopa <rumikorios@gmail.com>
2021-12-16 23:08:51 -0500

    create components folder and GetList File to separate concerns

---
+++ rickmortylist/src/components/GetList.js
@@ -0,0 +1,19 @@
+import React, { component } from 'react';
+import Proptypes, { component } from 'proptypes';
+
+GetList.proptypes = {
+
+};
+
+function GetList(props) {
+
+  return (
+
+    <div>
+
+    </div>
+  );
+
+};
+
+export default GetList;
