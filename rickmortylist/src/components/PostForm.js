57740b0a95414b7dfaa7746af6bca28dd261c1d5
rumikoacopa <rumikorios@gmail.com>
2021-12-16 23:24:17 -0500

    constructor to hold state, add value to value of input elements

--- rickmortylist/src/components/PostForm.js
+++ rickmortylist/src/components/PostForm.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 
 class PostForm extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       notes: ' '
+    }
+  }
+  
   render() {
+    const { notes } = this.state
     return (
       <div>
-        
+        <form>
+          <div>
+            <input type='text' name='note'></input>
+          </div>
+          
+        </form>
       </div>
-    )
+    );
   }
 }
 
