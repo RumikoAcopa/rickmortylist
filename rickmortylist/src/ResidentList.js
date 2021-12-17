8d03209e5e4a236beda6d3b9cf47c518539b5717
rumikoacopa <rumikorios@gmail.com>
2021-12-16 20:10:15 -0500

    created render that returns a div hello

--- rickmortylist/src/ResidentList.js
+++ rickmortylist/src/ResidentList.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 class ResidentList extends Component {
 
-  
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+
 }
 
 export default ResidentList;
