2da2d6b251fd3e909057153b268debeaf44be4d4
rumikoacopa <rumikorios@gmail.com>
2021-12-16 20:14:41 -0500

    constructor to hold state with empty list array

--- rickmortylist/src/ResidentList.js
+++ rickmortylist/src/ResidentList.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 
 class ResidentList extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: []
+    }
+  }
+
   render() {
     return (
       <div>Hello</div>
